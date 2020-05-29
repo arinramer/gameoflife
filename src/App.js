5a52e954ae506fbd3a0ab14bdb18ac321a89a0b4
arinramer <arinramer@gmail.com>
2020-05-28 19:18:37 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useRef} from 'react';
 import produce from 'immer';
 import { Button, Dropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CirclePicker } from 'react-color';
 
 const numRows = 50;
 const numCols = 50;
@@ -124,6 +125,8 @@ const operations = [
 ]
 
 function App() {
+  const [color, setColor] = useState("black")
+  const [colorToggled, setColorToggled] = useState(false)
   const [grid, setGrid] = useState(() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
@@ -165,65 +168,107 @@ const runSimulation = useCallback(() => {
   })  
   setTimeout(runSimulation, speed)
 }, [speed])
-
+const handleChangeComplete = (color) => {
+  setColor(color.hex);
+};
 return (
-  <div style={{display: 'flex', flexFlow: 'row', justifyContent: 'center', marginTop: '3%'}}>
-    <div style={{display: 'flex', flexFlow: 'column', marginRight: '5%'}}>
-      <Button variant={running ? "danger" : "primary"} style={{marginBottom: '12%'}} onClick={() => {
-        setRunning(!running);
-        if (!running) {
-          runningRef.current = true;
-          runSimulation();
-        }
-      }}>{running ? 'stop' : 'start'}</Button>
-      <Button variant="secondary" style={{marginBottom: '12%'}} onClick={() => {
-        setRunning(false);
-        setSpeed(1000)
-      }} disabled={(speed === 1000) ? "true" : ""}>Slow</Button>
-      <Button variant="secondary" style={{marginBottom: '12%'}} onClick={() => {
-        setRunning(false);
-        setSpeed(500)
-      }} disabled={(speed === 500) ? "true" : ""}>Medium</Button>
-      <Button variant="secondary" style={{marginBottom: '12%'}} onClick={() => {
-        setRunning(false);
-        setSpeed(100)
-      }} disabled={(speed === 100) ? "true" : ""}>Fast</Button>
-      <Dropdown style={{marginBottom: '12%'}}>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Presets
-        </Dropdown.Toggle>
+  <div style={{display: 'flex', flexFlow: 'column', alignItems: 'center'}}>
+    <h1 style={{margin: '2%'}}>Conway's Game of Life</h1>
+    <div style={{display: 'flex', flexFlow: 'row'}}>
+      <div style={{display: 'flex', flexFlow: 'column', marginLeft: '10%', marginRight: '-10%', width: '310px'}}>
+          <Button variant={running ? "danger" : "primary"} style={{marginBottom: '12%', width: '90px'}} onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}>{running ? 'stop' : 'start'}</Button>
+          <div style={{display: 'flex', flexFlow: 'column'}}>
+            <Button variant="secondary" style={{marginBottom: '12%', width: '90px'}} onClick={() => {
+              setRunning(false);
+              setSpeed(900)
+            }} disabled={(speed === 900) ? "true" : ""}>Slow</Button>
+            <Button variant="secondary" style={{marginBottom: '12%', width: '90px'}} onClick={() => {
+              setRunning(false);
+              setSpeed(500)
+            }} disabled={(speed === 500) ? "true" : ""}>Medium</Button>
+            <Button variant="secondary" style={{marginBottom: '12%', width: '90px'}} onClick={() => {
+              setRunning(false);
+              setSpeed(100)
+            }} disabled={(speed === 100) ? "true" : ""}>Fast</Button>
+          </div>
+          <div style={{display: 'flex', flexFlow: 'column'}}>
+            <Button variant="warning" style={{marginBottom: '12%', width: '90px'}} onClick={() => {
+              setRunning(false);
+              setGrid(() => {
+                const rows = [];
+                for (let i = 0; i < numRows; i++) {
+                  rows.push(Array.from(Array(numCols), () => 0))
+                }
+                return rows
+              })}
+            }>Reset</Button>
+            <Dropdown style={{marginBottom: '12%'}}>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Presets
+              </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={() => setGrid(pulsar)}>Pulsar</Dropdown.Item>
-          <Dropdown.Item onClick={() => setGrid(penta)}>Penta-decathlon</Dropdown.Item>
-          <Dropdown.Item onClick={() => setGrid(pulsar)}>Random</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => setGrid(pulsar)}>Pulsar</Dropdown.Item>
+                <Dropdown.Item onClick={() => setGrid(penta)}>Penta-decathlon</Dropdown.Item>
+                <Dropdown.Item onClick={() => setGrid(() => {
+                  const rows = [];
+                  for (let i = 0; i < numRows; i++) {
+                    rows.push(Array.from(Array(numCols), () => Math.round(Math.random())))
+                  }
+                  return rows
+                })}>Random</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <Button variant="success" style={{marginBottom: '0%', width: '90px'}} onClick={() => setColorToggled(!colorToggled)}>{colorToggled ? "Hide Colors" : "Show Colors"}</Button>
+          
+        </div>
+      <div 
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 15px)`
+        }}>
+          {grid.map((rows, i) => 
+            rows.map((col, k) => ( 
+            <div 
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                })
+                setGrid(newGrid)
+              }}
+              style={{ 
+                width: 15, 
+                height: 15, backgroundColor: grid[i][k] ? color : undefined,
+                border: 'solid 1px black'}}
+              />
+          ))
+        )}
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column', marginLeft: '3%', width: '275px'}}>
+        <h1>Rules</h1>
+        <ul style={{marginBottom: '23%'}}>
+          <li>Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.</li>
+          <li>Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.</li>
+          <li>Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.</li>
+          <li>Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.</li>
+        </ul>
+        {colorToggled ? <CirclePicker onChangeComplete={ handleChangeComplete } /> : null}
+      </div>
+      
     </div>
-    <div 
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 15px)`
-      }}>
-        {grid.map((rows, i) => 
-          rows.map((col, k) => ( 
-          <div 
-            key={`${i}-${k}`}
-            onClick={() => {
-              const newGrid = produce(grid, gridCopy => {
-                gridCopy[i][k] = grid[i][k] ? 0 : 1;
-              })
-              console.log(newGrid)
-              setGrid(newGrid)
-            }}
-            style={{ 
-              width: 15, 
-              height: 15, backgroundColor: grid[i][k] ? 'lightgreen' : undefined,
-              border: 'solid 1px black'}}
-            />
-        ))
-      )}
-    </div>
+    <h4 style={{marginTop: '1%', marginBottom: '1%'}}>About this algorithm:</h4>
+    <p style={{width: '750px', marginBottom: '2%'}}>The game of life is a cellular automaton created by Cambridge mathematician John Conway in 1970. Conway's goal was to create an interesting cell automation
+      program from which complex patterns can emerge. The user can create a configuration of cells,
+      and once the program runs it calculates the next position of each cell by following the four rules listed above. Each new configuration of cells is called a generation.
+      The game of life is Turing Complete, meaning it can be used to solve any computation problem.</p>
   </div>
   )
 }
